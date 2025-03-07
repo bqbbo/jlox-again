8bd4cd52925ee1c5d7f10ba237fe6be3daff0bf6
bobbo <bobbye23899@gmail.com>
2025-03-07 04:03:29 +0000

    Scanner, Part One

---
+++ src/main/java/com/bqbbo/lox/Scanner.java
@@ -0,0 +1,23 @@
+package com.bqbbo.lox;
+
+import static com.bqbbo.lox.TokenType.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Scanner {
+    private final String sourceCode;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String sourceCode) {
+        this.sourceCode = sourceCode;
+    }
+
+    List<Token> scanTokens() {
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+}
